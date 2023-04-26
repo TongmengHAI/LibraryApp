79b4d316ce3a95f6330be731d08f72f7d4828eb5
Kang Eangchheang <eangchheangkang@gmail.com>
2023-04-26 22:20:04 +0700

    testing

--- src/samples/db/InsertData.java
+++ src/samples/db/InsertData.java
@@ -4,9 +4,9 @@ public class InsertData {
     public static void main(String[] args) {
         CreateProductTable.main(args);
         try (ConnectDB conn = ConnectDB.getConnection();) {
-            int ret = conn.executeUpdate("INSERT INTO products(name,price) VALUES(?,?)", "Grape",5.5);
+            int ret = conn.executeUpdate("INSERT INTO products(name,detail,author,public,type,price,quantity) VALUES(?,?,?,?,?,?,?)", "Grape book","goodbook","chhit",12,"horror",5.50,3);
             System.out.println(ret);
-            ret = conn.executeUpdate("INSERT INTO products(name,price) VALUES(?,?)", "Banana",0.5);
+            ret = conn.executeUpdate("INSERT INTO products(name,detail,author,public,type,price,quantity) VALUES(?,?,?,?,?,?,?)", "Banana book","goodbook1","mengtong",11,"scifi",90.5,2);
             System.out.println(ret);
             
         } catch (Exception e) {
